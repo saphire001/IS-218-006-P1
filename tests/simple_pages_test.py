c329c403ef51acb4383c5f14fccf64b2074f32a4
saanikajoshi <spj29@njit.edu>
2022-03-28 19:40:24 -0400

    undo

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -5,7 +5,7 @@ def test_request_main_menu_links(client):
     response = client.get("/")
     assert response.status_code == 200
     assert b'<a class="nav-link" href="/about">About</a>' in response.data
-    assert b'<a class="nav-link" href="/Pytest">Pytest</a>' in response.data
+    assert b'<a class="nav-link" href="/page1">Pytest</a>' in response.data
     assert b'<a class="nav-link" href="/page2">AAA Testing</a>' in response.data
     assert b'<a class="nav-link" href="/page3">OOP Terms</a>' in response.data
     assert b'<a class="nav-link" href="/page4">SOLID</a>' in response.data
