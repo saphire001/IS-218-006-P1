0b651746f3ea54dc9c396ca603cbcd4f6edb9995
saanikajoshi <spj29@njit.edu>
2022-03-28 19:47:47 -0400

    Nav links 3

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -32,19 +32,19 @@ def test_request_page2(client):
     """This makes the index page"""
     response = client.get("/page2")
     assert response.status_code == 200
-    assert b"Page 2" in response.data
+    assert b"AAA Testing" in response.data
 
 def test_request_page3(client):
     """This makes the index page"""
     response = client.get("/page3")
     assert response.status_code == 200
-    assert b"Page 3" in response.data
+    assert b"OOP Terms" in response.data
 
 def test_request_page4(client):
     """This makes the index page"""
     response = client.get("/page4")
     assert response.status_code == 200
-    assert b"Page 4" in response.data
+    assert b"SOLID" in response.data
 
 def test_request_page_not_found(client):
     """This makes the index page"""
